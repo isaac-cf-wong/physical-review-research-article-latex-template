02cbf1399c452f54c1e54ec9054124a1108f3b6a
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-03-22 15:02:39 +0100

    chore: Improve the setup script to get the new repo name automatically

--- setup_repo.sh
+++ setup_repo.sh
@@ -5,17 +5,10 @@ OLD_OWNER_NAME="isaac-cf-wong"
 OLD_REPO_NAME="physical-review-research-article-latex-template"
 OLD_REPO_NAME_NORMALIZED="${OLD_REPO_NAME//-/_}"
 
-# 1. Prompt the user for the new owner name
-read -p "Enter the new owner name: " NEW_OWNER_NAME
+url=$(git config --get remote.origin.url)
 
-# 2. Prompt the user for the new package name
-read -p "Enter the new package name: " NEW_REPO_NAME
-
-# 3. Check if the user actually entered something (Safety check)
-if [ -z "$NEW_REPO_NAME" ]; then
-    echo "Error: No package name entered. Exiting."
-    exit 1
-fi
+NEW_OWNER_NAME=$(echo "$url" | sed -E 's#.*[:/]([^/]+)/([^/]+)\.git#\1#')
+NEW_REPO_NAME=$(echo "$url" | sed -E 's#.*[:/]([^/]+)/([^/]+)\.git#\2#')
 
 NEW_REPO_NAME_NORMALIZED="${NEW_REPO_NAME//-/_}"
 
@@ -28,11 +21,7 @@ else
 fi
 
 # 4. Check if the owner is set. If not, use the original name.
-if [ -z "$NEW_OWNER_NAME" ]; then
-    echo "Warning. Owner name is empty, use the original owner name."
-else
-    sed "${SED_INPLACE[@]}" "s/$OLD_OWNER_NAME/$NEW_OWNER_NAME/g" cliff.toml
-fi
+sed "${SED_INPLACE[@]}" "s/$OLD_OWNER_NAME/$NEW_OWNER_NAME/g" cliff.toml
 
 find . -type d \( -name ".git" -o -name "__pycache__" -o -name ".venv" -o -name "node_modules" -o -name ".pytest_cache" \) -prune \
     -o -type f \( -name "*.py" -o -name "*.md" -o -name "*.toml" -o -name "LICENSE" \) \
